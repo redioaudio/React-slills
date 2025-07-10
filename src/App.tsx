014ea4a9fe845a63711b99f5d22277a1ad315fdb
j103806 <j103806@naver.com>
2025-07-10 14:50:41 +0900

    [Feature]React hook 학습  usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    //useState => Hooks
+    //useState는 가장 기본적인 Hook이며 함수 컴포넌트에서도 가변적인 상태를 지닐수 있게 해준다
+    // 이 함수가 호출되면 배열을 반환하나 ->첫번쨰 요소는 상태 값 두번째 요소는 상태를 설정하는 함수
+    //=>useState 함수의 파라미터에는 상태의 기본값을 넣어 준다
+    const [value, setValue] = useState(0);
+    const [name, setName] = useState<string>("스나이퍼팩토리");
+    const [nickname, setNickname] = useState<string>("상담잘하셧어요");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const increment = () => setValue(value + 1);
+    const decrement = () => setValue(value - 1);
+
+    const onChangeName = (event) => {
+        setName(event.target.value);
+    };
+    const onChangeNickname = (event) => {
+        setNickname(event.target.value);
+    };
+
+    return (
+        <div>
+            <p>
+                현재 카운터 값음 <b>{value}</b>
+            </p>
+            <button onClick={increment}>1증가</button>
+            <button onClick={decrement}>1감소</button>
+
+            <div>
+                <input type="text" value={name} onChange={onChangeName} />
+                <input type="text" value={nickname} onChange={onChangeNickname} />
+            </div>
+            <div>
+                <b>이름: {name} </b>
+                <b>닉네임: {nickname}</b>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
